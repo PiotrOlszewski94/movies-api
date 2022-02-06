19593096b678117b734a3ee4b56868328c0e33a5
Piotr Olszewski <piolszewski94@gmail.com>
2022-02-06 17:11:04 +0100

    added migrations

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn carsbackend.wsgi --log-file -
--- moviesapi/settings.py
+++ moviesapi/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#oq*#)t%$x5+_+co&6$8%+d(6*l-q6hz@25x+b3!#%%@n6zs#1'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
